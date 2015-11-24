1a4ac3a47bb5607c11f14e028f51d577bc277e36
Rich Gwozdz <rgwozdz@spatialdev.com>
2015-11-24 15:22:38 -0800

    Document and add port argument.

--- app.js
+++ app.js
@@ -1,8 +1,22 @@
+"use strict";
+
+
+// Dependencies
 var express = require('express');
 var compression = require('compression')
 var bodyParser = require('body-parser');
 var cors = require("cors");
 var path = require('path');
+var argv = require('minimist')(process.argv.slice(2));
+
+var port = argv.port || null;
+
+
+// Check for valid --port argument
+if (!(typeof port === 'number') && (port % 1 === 0) && (port > 0) && (port <= 65535)) {
+    console.error('Please provide a valid --port argument');
+    process.exit(1);
+}
 
 // Create the express instance
 var app = express();
@@ -24,20 +38,22 @@ app.use('/exports', express.static(path.join(__dirname, 'exports')));
 var endpoint = require('./endpoint.js');
 app.use('/url2png', endpoint);
 
-app.use(function(err, req, res, next) {
+// Error handler
+app.use(function (err, req, res, next) {
 
     console.error(err, err.stack);
     res.status(err.status || 500).json({
         message: err.message,
         error: err
     });
+
 });
 
 // START THE SERVER
 // =============================================================================
-app.listen(8999, function(){
+app.listen(port, function () {
 
-    console.log('API listening on port 8999.');
+    console.log('url2png API listening on port ' + port + '.');
 
 });
 
--- endpoint.js
+++ endpoint.js
@@ -1,3 +1,5 @@
+"use strict";
+
 var express = require('express');
 var router = express.Router();
 var banquo = require('banquo');
@@ -5,18 +7,22 @@ var fs = require('fs');
 var path = require('path');
 var CronJob = require('cron').CronJob;
 
+var PNGEXPORTDIRECTORY = 'exports';
+
 // Cron job to clear out exports folder;
 new CronJob('00 00 00 * * *', function(){
 
-    var files = fs.readdirSync('exports', function(err, files) {});
+    // Get files in export directory
+    var files = fs.readdirSync(PNGEXPORTDIRECTORY);
 
-    files.filter(function(filename) {
+    // Filter list of files to those with .png extension and then delete
+    files.filter(function (filename) {
 
         return path.extname(filename) === '.png';
 
     }).forEach(function(filename) {
         // Delete file
-        fs.unlink('exports/' + filename, function (err) {
+        fs.unlink(PNGEXPORTDIRECTORY + filename, function (err) {
             if (err) throw err;
             console.log('successfully deleted ' + filename);
         });
@@ -25,20 +31,24 @@ new CronJob('00 00 00 * * *', function(){
 }, null, true, "America/Los_Angeles");
 
 
-
+// Add the POST endpoint for generating a PNG from a URL
 router.post('/',  function (req, res, next) {
 
+    // Get POST data
     var url = req.body.url || null;
+    var viewportWidth = req.body.viewport_width || 1440;
+    var delay = req.body.delay || 5000;
 
+    // If no url, return 400 error
     if(!url) {
         res.status(400).json({message:"No URL to print in post data."});
         return;
     }
 
-    // We are going to capture the web page as a PNG and save it with a random name in the /exports directory
+    // We are going to capture the web page as a PNG and save it with a random name in the designated export directory
     var randomString = Math.random().toString(36).substr(2, 9);
     var timestamp = Date.now();
-    var filePath ="/exports/" + randomString + timestamp + '.png';
+    var filePath ="/" + PNGEXPORTDIRECTORY + randomString + timestamp + '.png';
 
     // Print options
     var opts = {
@@ -57,10 +67,11 @@ router.post('/',  function (req, res, next) {
             res.status(500).json({message:err.message, error:err});
             return;
         }
+
+        // Success; send 200 and the URL of the image. Will be deleted with Cron job.
         res.status(200).json({message:'success', downloadURL: opts.downloadURL});
     });
 
-    });
-
+});
 
 module.exports = router;
